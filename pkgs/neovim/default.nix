352768d5c5a57610572b9ecfd3ccfa9b37fe2bee
Ankit <gt3.someday@gmail.com>
2025-09-11 16:23:04 -0700

    switch to nightly

--- pkgs/neovim/default.nix
+++ pkgs/neovim/default.nix
@@ -5,17 +5,17 @@
 }:
 stdenv.mkDerivation rec {
   name = "nvim-linux-x86_64";
-  version = "v0.11.4";
+  version = "nightly";
   src = fetchzip {
     url = "https://github.com/neovim/neovim/releases/download/${version}/${name}.tar.gz";
-    # sha256 = "D95uJHRy/B/AJi2bCmFh8PZnr/Dm7FWmrvgkPAUiLcE=";
-    sha256 = "IDOEnV4oLHptb0DJ0uTsV9l0JJMzccQpvS4MYlBSeKk=";
+    sha256 = "b52106a73c15c2b9ae3ec0de2a32ff7f1e3c2991af21243e7a3935e5907dd7f7";
+    # sha256 = "IDOEnV4oLHptb0DJ0uTsV9l0JJMzccQpvS4MYlBSeKk=";
   };
   installPhase = ''
     cp -r $src/. $out
   '';
   meta = with lib; {
-    description = "Neovim latest";
+    description = "Neovim nightly";
     homepage = "https://github.com/neovim/neovim";
   };
 }
