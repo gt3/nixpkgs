16745d4528b57435a2e0c7640c57ffa29e531277
gt3 <gt3.someday@gmail.com>
2025-11-15 23:06:30 -0800

    pyrefly pypi setup

--- pkgs/pyrefly/default.nix
+++ pkgs/pyrefly/default.nix
@@ -1,6 +1,6 @@
 {
   pkgs,
-  fetchPypi,
+  fetchurl,
   lib
 }:
 with pkgs.python314Packages;
@@ -11,11 +11,13 @@ buildPythonPackage rec {
   # pyproject = true;
   format = "wheel";
   python = "py3";
+  abi = "none";
   platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
-  src = fetchPypi {
-    inherit pname version format python platform;
-    # sha256 = "37d0c8e0aca9c693574304daa076267a732560521ee470743bdd2fe14f272175";
-    sha256 = "f28c80d6b32758f2f15cf59b148b469a889c557e13cde8042e38bb8fe4c211da";
+  sha256 = "37d0c8e0aca9c693574304daa076267a732560521ee470743bdd2fe14f272175";
+  hash = "f2/8c/80d6b32758f2f15cf59b148b469a889c557e13cde8042e38bb8fe4c211da"
+  url = "https://files.pythonhosted.org/packages/${hash}/${pname}-${version}-${python}-${abi}-${platform}.whl";
+  src = fetchurl {
+    inherit url sha256
   };
   doCheck = false;
   meta = with lib; {
