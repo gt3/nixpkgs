8790ade3a18d352f7f891b72b7c033cf83965d82
gt3 <gt3.someday@gmail.com>
2025-11-15 23:12:33 -0800

    pyrefly pypi setup

--- pkgs/pyrefly/default.nix
+++ pkgs/pyrefly/default.nix
@@ -13,7 +13,8 @@ buildPythonPackage rec {
   python = "py3";
   abi = "none";
   platform = "manylinux_2_17_x86_64.manylinux2014_x86_64";
-  sha256 = "37d0c8e0aca9c693574304daa076267a732560521ee470743bdd2fe14f272175";
+  # sha256 = "37d0c8e0aca9c693574304daa076267a732560521ee470743bdd2fe14f272175";
+  sha256 = "a+tB9gJQNsCY/QqYu5099gK5c1Mv5NGkE7Xb8Kb/DoM=";
   hash = "f2/8c/80d6b32758f2f15cf59b148b469a889c557e13cde8042e38bb8fe4c211da";
   url = "https://files.pythonhosted.org/packages/${hash}/${pname}-${version}-${python}-${abi}-${platform}.whl";
   src = fetchurl {
